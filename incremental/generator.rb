81ed09b8e13d324e3c5c37b9167bbabcd0963d66
kasei_san <kasei.san@gmail.com>
2019-05-02 16:56:01 +0900

    Add

---
+++ incremental/generator.rb
@@ -0,0 +1,32 @@
+eval$s=%w'
+k=0;
+ns=[
+"BAhsKxnAfwD/H/z/5////vHvD/7+4O8P/v7g7w/+/uDvH//+/8//f/D/AfwH",
+"BAhsKxnAHwD+AfAfAP8BgB8A+AGAHwD4AYAfAPgBgB8A+AHw/wD/D/D/AP8P",
+"BAhsKxn+//D/P///9wd/APAHAH8A/gP/H/x/4P8AfwDwBwB/wP//////////",
+"BAhsKxn8/+H/f/7/7w/+AOAPAP7g/wf+P+D/BwD+AOAPAP7+4O////7/x/8f",
+"BAhsKxkA/wH4H8D/Af4f8PuBnx/8+OH/f/7/5/9/APgBgB8A+AHgfwD+B+B/",
+"BAhsKxn+/+f/f/7/5/9//gDgDwD+/+H/P/7/BwD+AODvD/7+/8//f/D/AfwH",
+"BAhsKxkA/gHwD4B/APwD4B8A/wD4B8D/D/7/4/9//uDvD/7+/8//f/D/AfwH",
+"BAhsKxn/////////////APgHwD8A/gHwD4B/APwD4B8A/wD4B8A/AP4B8A8A",
+"BAhsKxnAfwD/H/z/5////uDvD/78/4f/P/z/5w/+/uDvD/7+/8//f/D/AfwH",
+"BAhsKxnAfwD/H/z/5////uDvD/78/4///+D/B8A/AP4B8A+AfwD8A+AfAP8A"
+];
+n=ns[k];
+n=Marshal.load(n.unpack("m")[0]);
+o="";
+e="eval$s=%w"<<39<<($s*2);
+j=-1;
+0.upto(70){o<<e[j+=1]};o<<10;
+0.upto(16){|y|
+  0.upto(25){o<<e[j+=1]};
+  0.upto(19){|i|o<<((n[20*y+i]==1)?32:e[j+=1])};
+  0.upto(25){o<<e[j+=1]};
+  o<<10;
+};
+0.upto(70){o<<e[j+=1]};o<<10;
+o[-6,6]=""<<39<<".join";
+o[12] = ((o[12].to_i+1)%10).to_s;
+puts(o);
+#
+'.join
