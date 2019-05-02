d5cf12efaa568e92a542da9186d8622e2f0ecf83
kasei_san <kasei.san@gmail.com>
2019-05-02 15:10:27 +0900

    Add quine_aa/main.rb

---
+++ quine_aa/main.rb
@@ -0,0 +1,75 @@
+     eval$s=%w'     
+   b="BAhsK2Lgf4    
+ D/D/7/8/9///j/B/9  
+/8P8H/3/w/wf/f/7/7/ 
+///+//P/   j/Af5/AOA
+PAPwAAM     CHH3z4wY
+cffPjBh     x98+MGHH
+/z/wf8/     /P+D/z/g
+nwMAAAA     PAPgBAA8
+AAADAPw     D8A4A/AP
+gDgB8A+  AGAHwD4AeA/
+AP4D4D8A /gMAAOD8A/7
+/4P8f/v/D/z/88MMPP/z
+ www8//PDDDz/88AMA  
+   APD/wP8//v//D/   
+     7/////f///8x8A 
+             /wPg/z/
+              8/wP/P
+                    
+  wA=";    n=Mars   
+  hal.l    oad(b.   
+  unpac    k("m")   
+  [0]);    x=20;y   
+  =74;e    ="eval   
+  $s=%w    "<<39<   
+  <($s*    3);o="   
+  ";j=-1;0.upto(x   
+  *y){|i|o<<((n[i]  
+  ==1)?e[j+=1]:32)  
+   ;o<<((i%x==x-1)  
+     ?10:"")}  ;o[  
+                    
+        -10,        
+       6]=""<       
+        <39<        
+                    
+      <".join"      
+      ;puts(o)      
+       ;#b="BA      
+       hsK2Lgf      
+       4D/D/7       
+       /8/9//       
+       /j/B/9       
+       /8P8H/       
+     3/w/wf/f/      
+     7/7////+/      
+     /P/j/Af5/      
+     AOAPAPwAA      
+                    
+ MCH  H3z4wYcf      
+ fPjBhx98+MGHH/z    
+ /wf8//P+D/z/gnwM   
+ AAAAPAPgBAA8AAADA  
+  PwD8A4A/APgDgB8A  
+  +AGAHw    D4AeA/  
+  AP4D4D    8A/gMA  
+  AOD8A/    7/4P8f  
+  /v/D/z    /88MMP  
+  P/zwww    8//PDD  
+  Dz/88A    MAAPD/  
+  wP8//v    //D/7/  
+                    
+    ////f///8x8A    
+  /wPg/z/8/wP/PwA=  
+ ";n=Marshal.load(b.
+unpack("     m")[0])
+;x=20;y=74;e="eval$s
+=%w"<<39<<($s*3);o= 
+"";j=-1;0.upto(x*y  
+){|i|o<<(           
+(n[i]==1)?          
+ e[j+=1]:32);o<<((  
+  i%x==x-1)?10:"")  
+    };o[-10,'.join  
+
